a3d3847be4af31ccb236ec2099415df9d51673e0
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-02-09 10:46:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort by Set Bit Count/sort-by-set-bit-count.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+class Solution:
+    def sortBySetBitCount(self, arr, n):
+        # Your code goes here
+        arr.sort(key=lambda x: -bin(x).count('1'))
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        arr = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        ob.sortBySetBitCount(arr, n)
+        print(*arr)
+
+        T -= 1
+
+
+        print("~")
+if __name__ == "__main__":
+    main()
+
+
+
+# } Driver Code Ends
