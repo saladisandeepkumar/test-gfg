0d9ffc34cd5cb0012fe4e8095ab22e5898439a0a
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-02-23 16:11:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Dictionary in Python - III/dictionary-in-python-iii.py
@@ -0,0 +1,81 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+# insert into dictionary
+def insert_dict(query, dict):
+    
+    # Your code here
+    dict.update({query[1]:query[2]})
+
+    
+
+# deleting from dictionary
+def del_dict(query, dict):
+    
+    # Your code here
+    if query[1] in dict.keys():
+        # print("yes")
+        del dict[query[1]]
+
+
+    
+
+# print marks of required name
+def print_dict(key, dict):
+    
+    # Your code here
+     if key in dict.keys():
+         print("Marks of %s is %s" %(key,dict[key]))
+         
+     else:
+         return False
+    # Your code here
+    
+    
+    
+
+#{ 
+ # Driver Code Starts.
+# Driver code
+def main():
+    # testcase input
+    testcase = int(input())
+    
+    # looping through testcases
+    while(testcase > 0):
+        
+        N = int(input())
+        
+        i = 0
+        dict = {}
+        while i < N:
+            flag = False
+            delete = False
+            query = input().split()
+            if(query[0] == 'i'):
+                insert_dict(query, dict)
+                print ("Inserted")
+            
+            if(query[0] == 'd'):
+                if del_dict(query, dict) is False:
+                    print ("-1")
+                else:
+                    print ("Deleted")
+            
+            if(query[0] == 'p'):
+                if(print_dict(query[1], dict) is False):
+                    print ("-1")
+                    
+            i+=1
+            
+        testcase -= 1
+
+
+        print("~")
+if __name__ == '__main__':
+    main()
+# } Driver Code Ends
