0fcdef40f1c057e824ee898f89db351f4f2af951
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-02-24 19:34:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print With Space/print-with-space.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+void utility() {
+    string a, b;
+    cin >> a >> b;
+    // Write your code below that prints a <space> b
+    cout<<a<<" "<<b<<endl;
+}
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        utility();
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
