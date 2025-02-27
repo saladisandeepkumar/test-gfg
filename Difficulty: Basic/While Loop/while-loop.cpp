ecdaf8e1551e6def0d097ffac1cfa77c0be2e708
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-02-27 09:26:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//User function Template for C++
+
+void utility(int x){
+    while(x>=0){
+       cout<<x<<" ";
+       x--;
+   }
+}
+
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	
+	while(t--){
+	    int x; cin>>x;
+        utility(x);
+        cout<<endl;
+	
+cout << "~" << "\n";
+}
+}
+// } Driver Code Ends
