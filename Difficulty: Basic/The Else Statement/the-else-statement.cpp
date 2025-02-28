d4bcb2395ed47b5e459e2e27f7ae66bd3d054717
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-02-28 21:24:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else Statement/the-else-statement.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//User function Template for C++
+
+void utility(int number){
+    
+    //Write your if-else statement below
+    if(number>100)
+    {
+            cout<<"Big"<<endl;
+    }
+    else 
+       { cout<<"Number"<<endl;
+           
+       }
+    
+    //Write your if-else statement above
+    
+}
+
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	while(t-- > 0) {
+        int number; cin>>number;
+        utility(number);
+	
+cout << "~" << "\n";
+}
+	
+}
+// } Driver Code Ends
