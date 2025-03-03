a27c102d33955c4e835db9741873230842dcac88
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-03-03 21:14:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Concatenate Integers/concatenate-integers.cpp
@@ -0,0 +1,19 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    // Write Your Code to Concate and Print
+    cout<<a<<b;
+
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
