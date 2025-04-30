58b7a4a53b6edc395eb97646bd2b107ea78d9969
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-04-30 19:35:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.py
@@ -0,0 +1,69 @@
+'''
+	Function Arguments: head of the original list.
+	Return Type: head of the new list formed.
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}'''
+	
+class Solution:
+    def segregate(self, head):
+        #code here
+        array=[]
+        
+        while head:
+            array.append(head.data)
+            head=head.next
+            
+        array.sort()
+        
+        newhead=Node(array[0])
+        head=newhead
+        
+        i=1
+        while i<len(array):
+            newhead.next=Node(array[i])
+            newhead=newhead.next
+            i+=1
+        return head
+    
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def printList(node):
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+        head = None
+        if arr:
+            head = Node(arr[0])
+            tail = head
+            for value in arr[1:]:
+                tail.next = Node(value)
+                tail = tail.next
+
+        printList(Solution().segregate(head))
+        print("~")
+
+# } Driver Code Ends
