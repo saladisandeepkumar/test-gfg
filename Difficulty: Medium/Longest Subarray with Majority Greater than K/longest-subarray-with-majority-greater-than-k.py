de5c893902b9b671566d77cf13f4737488825800
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-08-02 19:38:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
@@ -1,44 +1,16 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
-
-#User function Template for python3
-from collections import defaultdict
 class Solution:
     def longestSubarray(self, arr, k):
+        from itertools import accumulate
+        arr = [-1 if e <= k else 1 for e in arr]
+        acc = accumulate(arr)
+        m, running, ans = {}, 0, 0
+        for i, e in enumerate(acc):
+            if e > 0: # e is already > 0, 0..i is good
+                ans = max(ans, i+1) 
+            if e not in m:
+                m[e] = i
+            if e-1 in m:
+                ans = max(ans, i-m[e-1])
+        return ans
         # Code Here
-        sm = 0
-        res = 0
-        dc = defaultdict(int)
-        dc[0] = -1
-        for ind, num in enumerate(arr):
-            sm += 1 if num >k else -1
-            if sm > 0:
-                res = max(res, ind+1)
-            else:
-                if sm not in dc:
-                    dc[sm] = ind
-                if sm-1 in dc:
-                    res = max(res, ind - dc[sm-1])
-            
-        return res
-                
-
-
-#{ 
- # Driver Code Starts.
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
         
-        arr = [int(x) for x in input().strip().split()]
-        k = int(input())
-        
-        ob = Solution()
-        print(ob.longestSubarray(arr, k))
-        print("~")
-        t -= 1
-# } Driver Code Ends
